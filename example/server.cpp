5dce2fd7a1b295b38d992f3c590dcbac11ecd6ca
joao41 <jfigueiredo095@gmail.com>
2025-09-11 13:26:12 +0100

    The server already respondes to a http request

--- example/server.cpp
+++ example/server.cpp
@@ -6,11 +6,12 @@
 /*   By: jperpct <jperpect@student.42porto.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 09:53:00 by jperpct           #+#    #+#             */
-/*   Updated: 2025/09/10 11:03:54 by jperpct          ###   ########.fr       */
+/*   Updated: 2025/09/10 11:52:33 by jperpct          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
+#include <asm-generic/socket.h>
 #include <iostream>
 #include <memory_resource>
 #include <ostream>
@@ -19,7 +20,7 @@
 #include <netinet/in.h>
 #include <unistd.h>
 
-#define PORT 8080
+#define PORT 10000
 
 
 int main()
@@ -35,6 +36,14 @@ int main()
 	{
 		std::cout << "error: not open socket" << std::endl;
 		exit(1);
+	}
+
+	int opt = 1;
+	if(setsockopt(server_fd,SOL_SOCKET,SO_REUSEADDR,&opt,sizeof(opt)) < 0)
+	{
+		
+		std::cout << "error: the restor the port not setd" << std::endl;
+		exit(EXIT_FAILURE);
 	}
 	
 	address.sin_family = AF_INET;
@@ -67,6 +76,28 @@ int main()
 	 read(new_socket, buffer, 1024);
 
 		std::cout << "mensagem recebida: " << buffer  << std::endl;
+
+
+    	std::string resposta_http =
+        "HTTP/1.1 200 OK\n"
+        "Date: Thu, 11 Sep 2025 14:23:00 GMT\n"
+        "Server: Apache/2.4.54 (Ubuntu)\n"
+        "Content-Type: text/html; charset=UTF-8\n"
+        "Content-Length: 137\n"
+        "\n"
+        "<!DOCTYPE html>\n"
+        "<html>\n"
+        "<head>\n"
+        "    <title>Exemplo</title>\n"
+        "</head>\n"
+        "<body>\n"
+        "    <h1>Olá, mundo!</h1>\n"
+        "    <h1>Olá, mundo!</h1>\n"
+	" olas "
+        "</body>\n"
+        "</html>\n";
+	
+	send(new_socket,resposta_http.c_str(),resposta_http.size(),0);
 
 	close(new_socket);
 	close(server_fd);

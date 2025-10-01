5dfe120639bb03a8a3da8b58b5ee9da7068e3be3
jose5556 <josefilmeneses@gmail.com>
2025-10-01 17:16:49 +0100

    simple methods and atributes to use later

--- include/net/Socket.hpp
+++ include/net/Socket.hpp
@@ -6,13 +6,15 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/15 16:58:14 by joseoliv          #+#    #+#             */
-/*   Updated: 2025/09/25 19:22:43 by joseoliv         ###   ########.fr       */
+/*   Updated: 2025/10/01 17:10:23 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
 
 class Socket {
 	
@@ -23,4 +25,11 @@ public:
 	Socket& operator=(const Socket& src);
 
 	void	close();
+
+	int					getFd() const;
+	struct sockaddr_in	getAddr() const;
+
+private:
+	struct sockaddr_in	_addr;
+	int					_fd;
 };

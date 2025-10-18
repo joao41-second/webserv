644067cd158dc88b0b0b56fadcef0a011c6d7cef
jose5556 <josefilmeneses@gmail.com>
2025-10-18 17:04:16 +0100

    created a temporary contiguos array, and implemented the accept function inside the loop. The server is running properlly

--- srcs/core/EventLoop.cpp
+++ srcs/core/EventLoop.cpp
@@ -6,7 +6,7 @@
 /*   By: cereais <cereais@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/25 19:35:11 by joseoliv          #+#    #+#             */
-/*   Updated: 2025/10/15 20:03:12 by cereais          ###   ########.fr       */
+/*   Updated: 2025/10/18 16:55:05 by cereais          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,21 @@
 #include "../../include/core/Server.hpp"
 #include "../../include/core/Connection.hpp"
 
-EventLoop::EventLoop() {
-	_pollEntries.reserve(1024);
-}
+EventLoop::EventLoop() { _pollEntries.reserve(1024); }
 
 EventLoop::~EventLoop()
 {
-
 	for (size_t i = 0; i < _pollEntries.size(); ++i)
 	{
 		if (_pollEntries[i].conn)
 			delete _pollEntries[i].conn;
-		close(_pollEntries[i].pfd.fd);
+		if (_pollEntries[i].pfd.fd >= 0)
+			close(_pollEntries[i].pfd.fd);
 	}
 }
 
 void EventLoop::addListeningSocket(const Socket *socket, Server &server)
 {
-
 	struct pollfd pfd;
 	pfd.fd = socket->getFd();
 	pfd.events = POLLIN;
@@ -48,108 +45,116 @@ void EventLoop::addListeningSocket(const Socket *socket, Server &server)
 
 void EventLoop::run()
 {
-
 	while (true)
 	{
-		if (!_pollEntries.empty())
-		{
-			int errorCode = poll(&_pollEntries[0].pfd, _pollEntries.size(), 5000);
-			if (errorCode < 0)
-			{
+		// build contiguous temp pollfd array
+		std::vector<struct pollfd> pfdArray;
+		std::vector<size_t> indexMap;
+		pfdArray.reserve(_pollEntries.size());
+		for (size_t i = 0; i < _pollEntries.size(); ++i) {
+
+			if (_pollEntries[i].pfd.fd >= 0) {
+				pfdArray.push_back(_pollEntries[i].pfd);
+				indexMap.push_back(i);
+			}
+		}
+
+		if (!pfdArray.empty()) {
+			int errorCode = poll(&pfdArray[0], pfdArray.size(), 5000);
+			if (errorCode < 0) {
 				perror("poll");
 				break;
 			}
-			for (size_t i = 0; i < _pollEntries.size(); ++i) {
-				sleep(1);
-    			std::cout << "fd " << _pollEntries[i].pfd.fd 
-              << " events=" << _pollEntries[i].pfd.events 
-              << " revents=" << _pollEntries[i].pfd.revents << std::endl;
-			}
+			for (size_t j = 0; j < pfdArray.size(); ++j)
+				_pollEntries[indexMap[j]].pfd.revents = pfdArray[j].revents;
 		}
-		for (size_t i = 0; i < _pollEntries.size(); ++i)
-		{
+		else
+			usleep(1000);
+
+		std::vector<PollEntry> newClients;
+
+		for (size_t i = 0; i < _pollEntries.size(); ++i) {
 			PollEntry &entry = _pollEntries[i];
 
-			if (entry.pfd.revents & (POLLHUP | POLLERR | POLLNVAL)) 
-			{
-				std::cout << "Strange error, wow" << std::endl;
-    			closeConnection(entry);
-    			continue;
+			if (entry.pfd.revents & (POLLHUP | POLLERR | POLLNVAL)) {
+				std::cout << "Strange error, but client was brutally disconected :(" << std::endl;
+				closeConnection(entry);
+				entry.pfd.fd = -1;
+				continue;
 			}
 
-			// ready to read
-			if (entry.pfd.revents & POLLIN)
-			{
-				std::cout << "pollin" << std::endl;
-				if (entry.conn == NULL)
-				{
-					handleNewConnection(entry);
-					break ;
-				}
-				else
-				{
-					std::cout << "else!!!" << std::endl;
-					if (!entry.conn->readRequest())
+			if (entry.pfd.revents & POLLIN) {
+				if (entry.conn == NULL) {
+
+					socklen_t addrLen = sizeof(entry.socketAddr);
+					int clientFd = accept(entry.pfd.fd, (sockaddr *)&entry.socketAddr, &addrLen);
+					if (clientFd < 0)
+						std::cerr << "Failed to accept connection" << std::endl;
+					else {
+						int flags = fcntl(clientFd, F_GETFL, 0);
+						fcntl(clientFd, F_SETFL, flags | O_NONBLOCK);
+
+						Connection *conn = new Connection(clientFd, *entry.server);
+						struct pollfd pfd;
+						pfd.fd = clientFd;
+						pfd.events = POLLIN;
+						pfd.revents = 0;
+
+						PollEntry clientEntry;
+						clientEntry.pfd = pfd;
+						clientEntry.conn = conn;
+						clientEntry.server = entry.server;
+						clientEntry.socketAddr = entry.socketAddr;
+
+						newClients.push_back(clientEntry);
+						std::cout << "New client accepted: fd " << clientFd << std::endl;
+					}
+				} else {
+
+					if (!entry.conn->readRequest()) {
 						closeConnection(entry);
-					else if (entry.conn->isRequestComplete())
-					{
+						entry.pfd.fd = -1;
+					}
+					else if (entry.conn->isRequestComplete()) {
 						std::cout << entry.conn->getReadBuffer() << std::endl;
 						entry.pfd.events = POLLIN;
 					}
 				}
 			}
-			/* sleep(3);
-			std::cout << entry.pfd.events << std::endl; */
-			if (entry.pfd.revents & POLLOUT)
-			{
-				std::cout << "POLLOUT" << std::endl;
-				if (entry.conn && !entry.conn->writeResponse())
+
+			if (entry.pfd.revents & POLLOUT) {
+				if (entry.conn && !entry.conn->writeResponse()) {
 					closeConnection(entry);
+					entry.pfd.fd = -1;
+				}
 			}
+
 			entry.pfd.revents = 0;
 		}
+
+		if (!newClients.empty())
+			_pollEntries.insert(_pollEntries.end(), newClients.begin(), newClients.end());
+
+		//remove entries with fd < 0 (closed)
+		std::vector<PollEntry> remaining;
+		remaining.reserve(_pollEntries.size());
+		for (size_t k = 0; k < _pollEntries.size(); ++k) {
+			if (_pollEntries[k].pfd.fd >= 0)
+				remaining.push_back(_pollEntries[k]);
+			else
+				if (_pollEntries[k].conn)
+					delete _pollEntries[k].conn;
+		}
+		_pollEntries.swap(remaining);
 	}
-}
-
-void EventLoop::handleNewConnection(const PollEntry &entry)
-{
-
-	//socklen_t addrLen = sizeof(entry.socketAddr);
-
-	int clientFd = accept(entry.pfd.fd, NULL, NULL);
-	std::cout << "Accepted client fd: " << clientFd 
-          << ", events=" << entry.pfd.events << std::endl;
-
-	if (clientFd < 0)
-	{
-		std::cerr << "Failed to accept connection\n";
-		return;
-	}
-
-	int flags = fcntl(clientFd, F_GETFL, 0);
-	fcntl(clientFd, F_SETFL, flags | O_NONBLOCK);
-
-	Connection *conn = new Connection(clientFd, *entry.server);
-
-	struct pollfd pfd;
-	pfd.fd = clientFd;
-	pfd.events = POLLIN;
-	pfd.revents = 0;
-
-	PollEntry clientEntry;
-	clientEntry.pfd = pfd;
-	clientEntry.conn = conn;
-	clientEntry.server = entry.server;
-
-	_pollEntries.push_back(clientEntry);
-	std::cout << "New client accepted: fd " << clientFd << std::endl;
 }
 
 void EventLoop::closeConnection(PollEntry &entry)
 {
-
-	close(entry.pfd.fd);
-	delete entry.conn;
+	if (entry.pfd.fd >= 0)
+		close(entry.pfd.fd);
+	if (entry.conn)
+		delete entry.conn;
 	entry.conn = NULL;
 	std::cout << "Connection closed: fd " << entry.pfd.fd << std::endl;
 }
